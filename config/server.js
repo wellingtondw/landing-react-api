0b013a85dd49358082e7e4827ab970f222ab7ac4
wellington <wellingtonslpbi@hotmail.com>
2020-11-20 14:28:32 -0300

    create project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b201a0f70da3ceb2716919adb0b87797'),
+    },
+  },
+});
